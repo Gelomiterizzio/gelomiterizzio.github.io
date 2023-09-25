216c3b4572f8f93b2a043118b9e4ff3418994733
Sewnats <108907135+Sewnats@users.noreply.github.com>
2023-09-25 09:37:49 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Función para activar/desactivar el menú hamburguesa
+document.getElementById("menu-icon").addEventListener("click", function () {
+    var nav = document.getElementById("nav");
+    nav.classList.toggle("active");
+});
